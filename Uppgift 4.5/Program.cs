e26ba379397adbeb6daa0fd650acc029ed22e72e
Souzan <Souzan@DESKTOP-VHKB7GI>
2023-03-04 14:28:17 +0200

    Add project files.

---
+++ Uppgift 4.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett heltal");
+        string heltal = Console.ReadLine();
+
+        for (int i = 0; i < heltal.Length; i++)
+        {
+            string tecken = heltal[i].ToString();
+            int siffra = int.Parse(tecken);
+            if (siffra == 9)
+            {
+                int x = 0;
+                Console.Write(x);
+            }
+            else
+            {
+                int y = siffra + 1;
+                Console.Write(y);
+            }
+        }
+    }
+}
